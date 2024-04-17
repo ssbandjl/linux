980acbf8daf5d793fe128ab0a207026f5de4d157
xb <ssbandjl@163.com>
2024-04-17 21:03:15 +0800

    add ib/vxworks note

---
+++ test/vxworks/vxworks_driver_test.c
@@ -0,0 +1,56 @@
+#include
+
+#include
+
+#include
+
+#include
+
+int main(int argc, char *argv[])
+
+{
+int fd;
+
+int ret;
+
+char *wbuf = "I Love embedded system!\n";
+
+char rbuf[256];
+
+fd = open("/dev/fsdev", O_RDWR, 0);
+
+if (fd < 0) {
+perror("fsdev");
+
+return -1;
+
+}
+
+ret = write(fd, wbuf, strlen(wbuf) + 1);
+
+if (ret < 0) {
+perror("fsdev");
+
+close(fd);
+
+return ret;
+
+}
+
+ret = read(fd, rbuf, sizeof(rbuf));
+
+if (ret < 0) {
+perror("fsdev");
+
+close(fd);
+
+return ret;
+
+}
+
+printf("read data: %s", rbuf);
+
+close(fd);
+
+}
+
