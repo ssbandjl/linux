e050cb013867017293eadd73810f3051bbf1c99c
xb <ssbandjl@gmail.com>
2024-07-08 11:13:08 +0800

    update qemu_vm install kernel

--- qemu_vm_install.sh
+++ qemu_vm_install.sh
@@ -2,8 +2,14 @@
 make modules_install
 make install
 echo -e """
+
 cat /boot/grub/grub.cfg
+grep submenu /boot/grub/grub.cfg
+grep 5.15 /boot/grub/grub.cfg
+
+
 vim /etc/default/grub
+REF: GRUB_DEFAULT=gnulinux-advanced-9cf7073a-6786–43dc-895e-8bbebe712a1e>gnulinux-4.15.0–136-lowlatency-advanced-9cf7073a-6786–43dc-895e-8bbebe712a1e
 add: GRUB_DEFAULT='Advanced options for Ubuntu>Ubuntu, with Linux 5.15.0'
 ref: GRUB_CMDLINE_LINUX_DEFAULT="console=tty1 console=ttyS0 nvme_core.io_timeout=4294967295 nokaslr"
 update-grub
