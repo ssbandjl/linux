567f862a501b032de0a28f3b84acd9f1404b5af1
xb <ssbandjl@163.com>
2024-07-09 22:28:39 +0800

    add vdpa, virtio, virtio_blk note

--- build.sh
+++ build.sh
@@ -2,5 +2,19 @@
 #         i. 选中Processor type and features
 #             1. 取消勾选EFI runtime service support（取消勾选这一项会自动给module签名）
 
+
+# apt-get install aptitude
+# sudo aptitude install zstd
 #make menuconfig
+make oldconfig
+make prepare
+make script
+# CONFIG_MODULE_SIG=n
 make -j64
+
+
+# sed -i s/CONFIG_SYSTEM_TRUSTED_KEYS.*/CONFIG_SYSTEM_TRUSTED_KEYS=\"\"/g .config
+# sed -i s/CONFIG_SYSTEM_REVOCATION_KEYS.*/CONFIG_SYSTEM_REVOCATION_KEYS=\"\"/g .config
+
+# make -j64
+# objcopy --only-keep-debug vmlinux kernel.sym
