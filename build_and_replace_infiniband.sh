37c67ee4bc048421966b6d1a34fa426bca3e85e9
xb <bin@sidynamic.com>
2025-05-28 11:22:21 +0800

    update

--- build_and_replace_infiniband.sh
+++ build_and_replace_infiniband.sh
@@ -1,6 +1,10 @@
+# cat ~/project/linux/v5.15/linux/Makefile | grep '^VERSION\|^PATCHLEVEL\|^SUBLEVEL'
+# uname -r
+
 cd drivers/infiniband/
+# make -C /lib/modules/$(uname -r)/build M=$(pwd) clean
 make -C /lib/modules/$(uname -r)/build M=$(pwd) modules
-ls -alh /lib/modules/$(uname -r)/build/
+# ls -alh /lib/modules/$(uname -r)/build/
 cp /lib/modules/5.15.0+/kernel/drivers/infiniband/core/ib_core.ko /lib/modules/5.15.0+/kernel/drivers/infiniband/core/ib_core.ko.bak
 cp /lib/modules/5.15.0+/kernel/drivers/infiniband/core/ib_uverbs.ko /lib/modules/5.15.0+/kernel/drivers/infiniband/core/ib_uverbs.ko.bak
 cp core/ib_core.ko /lib/modules/5.15.0+/kernel/drivers/infiniband/core/ib_core.ko
