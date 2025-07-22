00b46eb56de20b42f9ef6566c2333328847d1454
xb <bin@sidynamic.com>
2025-07-22 08:47:07 +0800

    u

--- build_and_replace_infiniband.sh
+++ build_and_replace_infiniband.sh
@@ -5,10 +5,10 @@ cd drivers/infiniband/
 # make -C /lib/modules/$(uname -r)/build M=$(pwd) clean
 make -C /lib/modules/$(uname -r)/build M=$(pwd) modules
 # ls -alh /lib/modules/$(uname -r)/build/
-cp /lib/modules/5.15.0+/kernel/drivers/infiniband/core/ib_core.ko /lib/modules/5.15.0+/kernel/drivers/infiniband/core/ib_core.ko.bak
-cp /lib/modules/5.15.0+/kernel/drivers/infiniband/core/ib_uverbs.ko /lib/modules/5.15.0+/kernel/drivers/infiniband/core/ib_uverbs.ko.bak
-cp core/ib_core.ko /lib/modules/5.15.0+/kernel/drivers/infiniband/core/ib_core.ko
-cp core/ib_uverbs.ko /lib/modules/5.15.0+/kernel/drivers/infiniband/core/ib_uverbs.ko
+cp /lib/modules/`uname -r`/kernel/drivers/infiniband/core/ib_core.ko /lib/modules/`uname -r`/kernel/drivers/infiniband/core/ib_core.ko.bak
+cp /lib/modules/`uname -r`/kernel/drivers/infiniband/core/ib_uverbs.ko /lib/modules/`uname -r`/kernel/drivers/infiniband/core/ib_uverbs.ko.bak
+cp core/ib_core.ko /lib/modules/`uname -r`/kernel/drivers/infiniband/core/ib_core.ko
+cp core/ib_uverbs.ko /lib/modules/`uname -r`/kernel/drivers/infiniband/core/ib_uverbs.ko
 cd -
 
 echo -e "Build and replace ib_core, ib_uverbs ok"
