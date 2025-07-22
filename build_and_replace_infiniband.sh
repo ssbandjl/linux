9db77b3c9c637f0d8b4f033a7642fd22bd35f1fd
xb <bin@sidynamic.com>
2025-07-22 10:29:06 +0800

    u

--- build_and_replace_infiniband.sh
+++ build_and_replace_infiniband.sh
@@ -7,8 +7,10 @@ make -C /lib/modules/$(uname -r)/build M=$(pwd) modules
 # ls -alh /lib/modules/$(uname -r)/build/
 cp /lib/modules/`uname -r`/kernel/drivers/infiniband/core/ib_core.ko /lib/modules/`uname -r`/kernel/drivers/infiniband/core/ib_core.ko.bak
 cp /lib/modules/`uname -r`/kernel/drivers/infiniband/core/ib_uverbs.ko /lib/modules/`uname -r`/kernel/drivers/infiniband/core/ib_uverbs.ko.bak
+cp /lib/modules/`uname -r`/kernel/drivers/infiniband/core/iw_cm.ko /lib/modules/`uname -r`/kernel/drivers/infiniband/core/iw_cm.ko.bak
 cp core/ib_core.ko /lib/modules/`uname -r`/kernel/drivers/infiniband/core/ib_core.ko
 cp core/ib_uverbs.ko /lib/modules/`uname -r`/kernel/drivers/infiniband/core/ib_uverbs.ko
+cp core/iw_cm.ko /lib/modules/`uname -r`/kernel/drivers/infiniband/core/iw_cm.ko
 cd -
 
 echo -e "Build and replace ib_core, ib_uverbs ok"
